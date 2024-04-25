5b00f7ebd8501c8a357e310389811fd7ec5dd07b
Armando Soares Sousa <armando.sousa@gmail.com>
2024-04-25 16:46:27 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -9,10 +9,13 @@ URL_EXEMPLO = 'https://raw.githubusercontent.com/armandossrecife/teste/main/spac
 # extrai nome e extensao do arquivo apontado pela URL
 nome_imagem, extensao_imagem = meu_programa.utilidades.extrair_nome_extensao_url(URL_EXEMPLO)
 
-# Cria um recurso que aponta para uma imagem
-imagem1 = meu_programa.get_recurso_imagem(minha_url=URL_EXEMPLO)
-
-# Aplica filtros na imagem apontada e cria novas imagens de acordo com cada filtro
-meu_programa.aplica_filtro_grayscale(minha_imagem=imagem1, nome=nome_imagem)
-meu_programa.aplica_filtro_black_and_white(minha_imagem=imagem1, nome=nome_imagem)
-meu_programa.aplica_filtro_edges(minha_imagem=imagem1, nome=nome_imagem)
+try:
+  # Cria um recurso que aponta para uma imagem
+  imagem1 = meu_programa.get_recurso_imagem(minha_url=URL_EXEMPLO)
+  
+  # Aplica filtros na imagem apontada e cria novas imagens de acordo com cada filtro
+  meu_programa.aplica_filtro_grayscale(minha_imagem=imagem1, nome=nome_imagem)
+  meu_programa.aplica_filtro_black_and_white(minha_imagem=imagem1, nome=nome_imagem)
+  meu_programa.aplica_filtro_edges(minha_imagem=imagem1, nome=nome_imagem)
+except Exception as ex:
+  print(f"Erro: {str(ex)}")
